c0b3a26813c67fded4f5e0fc335783704b1c51ae
Ryan J. Price <ryapric@gmail.com>
2020-08-17 01:26:38 -0500

    Got pagination aggregator working...
    ... for ONE API method; not sure yet if it'll work for any others. Also add rate-limit checks, but haven't hit that ceiling yet.

--- fresh_slack/wrappers.py
+++ fresh_slack/wrappers.py
@@ -1,10 +1,11 @@
 from slack import WebClient
 from slack.errors import SlackApiError, SlackClientError
 import logging
-# from typing import
+import time
+from typing import Any
 
 
-class Slack():
+class Slack:
     def __init__(self,
                  slack_name: str,
                  slack_api_token: str,
@@ -19,21 +20,77 @@ class Slack():
         try:
             self.client.api_test()
         except SlackApiError as err:
+            # Unwrap error to fit on one log line
             logging.error(str(err).replace('\n', ' '))
             raise err
         logging.info(f'Authenticated to {self.slack_name}!')
+        logging.info(f'Running initial data fetch...')
 
-        self.get_channel_info()
+        self.get_channel_metadata()
         self.get_channels_by_key()
 
 
+    def try_call(self, client_method, *args, **kwargs) -> Any:
+        '''
+        This is used to wrap the Slack client calls, to catch pagination,
+        rate-limiting errors, and perform general API error handling.
+        '''
+        self.check_rate_limit(client_method, *args, **kwargs)
+        response = self.check_pagination(client_method, *args, **kwargs)
+        return response
+    
 
-    def get_channel_info(self) -> None:
-        self.info = self.client.conversations_list(exclude_archived = True).data
+    def check_rate_limit(self, client_method, *args, **kwargs):
+        retries = 0
+        while True:
+            if retries >= 5:
+                logging.error(f'Calling `{client_method.__name__}` retried too many times; aborting')
+
+            # Retry from rate limiting
+            try:
+                response = client_method(*args, **kwargs)
+                break
+            except SlackApiError as err:
+                retries += 1
+                if err.response['error'] == 'ratelimited':
+                    # The `Retry-After` header will tell you how long to wait
+                    # before retrying; also buffer with an additional second
+                    delay = int(err.response.headers['Retry-After']) + 1
+                    logging.info(f'Rate-limited when calling `{client_method.__name__}`; retrying in {delay} seconds')
+                    time.sleep(delay)
+                    continue
+                else:
+                    raise err
+        # Returned response only needed for some other method calls
+        return response
+    
+    def check_pagination(self, client_method, data_key, *args, **kwargs):
+        '''
+        client_method: method callable to have *this* method call
+        data_key: key in response.data that you want
+        *args, **kwargs: passed to client_method
+        '''
+        response = self.check_rate_limit(client_method, *args, **kwargs)
+        # Continue collecting any paginated results
+        response_data = response.data[data_key]
+        if 'response_metadata' in response.data:
+            response.__iter__()
+            while response.data['response_metadata']['next_cursor'] != '':
+                logging.info(f'`{client_method.__name__}` returned paginated results; getting more...')
+                self.check_rate_limit(response.__next__)
+                response_data += response.data[data_key]
+        return response_data
+
+    def get_channel_metadata(self) -> None:
+        channel_data = self.try_call(
+            self.client.conversations_list,
+            data_key = 'channels',
+            exclude_archived = True)
+        self.channel_data = channel_data
     
     def get_channels_by_key(self) -> None:
-        self.channels_by_id = {x['id']: x['name'] for x in self.info['channels']}
-        self.channels_by_name = {x['name']: x['id'] for x in self.info['channels']}
+        self.channels_by_id = {x['id']: x['name'] for x in self.channel_data}
+        self.channels_by_name = {x['name']: x['id'] for x in self.channel_data}
 
 
 # # Retry from rate limiting
